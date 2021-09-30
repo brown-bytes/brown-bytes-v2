bba8bd9879085a7abf7ff458efce993e33b246f8
scott-huson <randomrandom623@gmail.com>
2021-09-30 16:59:46 -0400

    SSL Test

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
 const execSync = require('child_process').execSync;
+const https = require('https');
+const fs = require('fs');
 const usersRouter = require("./routes/user");
 const profileRouter = require("./routes/profile");
 const eventsRouter = require("./routes/event");
@@ -11,7 +13,7 @@ const postsRouter = require("./routes/post");
 const feedbacksRouter = require("./routes/feedback");
 const winston = require('winston')
 const app = express();
-const port = 8080;
+const port = 3000;
 const host = "127.0.0.1";
 
 // Make sure the DB is initialized
@@ -25,6 +27,11 @@ const corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
+
+app = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/brownbytes.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/brownbytes.org/fullchain.pem'),
+}, app);
 
 
 // Set up logger
